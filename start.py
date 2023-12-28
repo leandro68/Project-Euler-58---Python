a0cd0c5c2399929063c2c3f7aa9f66d3393ac5d8
Lean <leandrorodriguez68@gmail.com>
2023-12-28 10:01:26 -0300

    comments added in start.py

--- start.py
+++ start.py
@@ -1,19 +1,35 @@
-import sympy
+# We'll use isprime function from sympy lib
+from sympy import isprime
 
+# amount to add to actual number, for finding next diagonal number
 addition = 0
+# amount of odds, starting from 1 (number 1 in the center)
 odds = 1
+# amount of primes (number 1 does not count as prime)
 primes = 0
+# ratio (primes/odds)
 ratio = 1
+# current number
 number = 1
 
+# while ratio is not less than 10%
 while ratio >= 0.1:
+  # we must add this to the current number, to find the next diagonal number 
   addition += 2
+  # we must find 4 diagonal numbers for each new square
   for i in range(4):
+    # to find the new diagonal number:
     number += addition
+    # then we have a new odd 
     odds += 1
-    if sympy.isprime(number):
+    # and if that odd is a prime
+    if isprime(number):
+      # we add 1 to primes account
       primes += 1
+  # when the square is complete, find the ratio
   ratio = primes / odds
 
+# print the side lenght, this is the founded answer
 print('side lenght: ',addition+1)
+# print the ratio to check it is less than 10%
 print('ratio: ', ratio)
